a0592370f585cab8374bca8bdc778e7e24359f13
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-02 07:05:55 -0500

    Lazy

--- app/src/main/java/com/example/stores/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/stores/mainModule/viewModel/MainViewModel.kt
@@ -10,13 +10,16 @@ import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 
 class MainViewModel: ViewModel() {
-    private var stores: MutableLiveData<List<StoreEntity>>
     private var interactor: MainInteractor
 
     init {
         interactor = MainInteractor()
-        stores = MutableLiveData()
-        loadStores()
+    }
+
+    private val stores: MutableLiveData<List<StoreEntity>> by lazy {
+        MutableLiveData<List<StoreEntity>>().also {
+            loadStores()
+        }
     }
 
     fun getStores(): LiveData<List<StoreEntity>>{
