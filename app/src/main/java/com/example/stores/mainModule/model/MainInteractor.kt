790148b3dba0230e7bbb255da273d3ac2a3eb86b
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-02 07:21:49 -0500

    Model(Main): Metodos restantes

--- app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
@@ -7,24 +7,29 @@ import org.jetbrains.anko.uiThread
 
 class MainInteractor {
 
-    interface StoresCallback{
-        fun getStoresCallback(callback: MutableList<StoreEntity>)
-    }
-
-    fun getStoresCallback(stores: StoresCallback){
-        doAsync {
-            val storeList = StoreApplication.dataBase.storeDao().getAllStores()
-            uiThread {
-                stores.getStoresCallback(storeList)
-            }
-        }
-    }
-
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit){
         doAsync {
             val storeList = StoreApplication.dataBase.storeDao().getAllStores()
             uiThread {
                 callback(storeList)
+            }
+        }
+    }
+
+    fun deleteStore(storeEntity: StoreEntity, callback: (StoreEntity) -> Unit){
+        doAsync {
+            StoreApplication.dataBase.storeDao().deleteStore(storeEntity)
+            uiThread {
+                callback(storeEntity)
+            }
+        }
+    }
+
+    fun updateStore(storeEntity: StoreEntity, callback: (StoreEntity) -> Unit){
+        doAsync {
+            StoreApplication.dataBase.storeDao().updateStore(storeEntity)
+            uiThread {
+                callback(storeEntity)
             }
         }
     }
