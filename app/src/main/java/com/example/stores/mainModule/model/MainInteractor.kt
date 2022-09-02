e45c1a2bf6e9ee40311c133cad62d9c50aac96c2
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-02 12:48:01 -0500

    Primer consulta con Volley

--- app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
@@ -1,13 +1,28 @@
 package com.example.stores.mainModule.model
 
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 import com.example.stores.StoreApplication
 import com.example.stores.common.entities.StoreEntity
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.getStackTraceString
 import org.jetbrains.anko.uiThread
 
 class MainInteractor {
 
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit){
+        val url = "https://stores.free.beeceptor.com/my/api/path";
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
+            Log.i("peticon", response.toString())
+        }, {
+            it.printStackTrace()
+        })
+
+        StoreApplication.storeAPI.addToRequestQueue(jsonObjectRequest)
+    }
+
+    fun getStoresRoom(callback: (MutableList<StoreEntity>) -> Unit){
         doAsync {
             val storeList = StoreApplication.dataBase.storeDao().getAllStores()
             uiThread {
