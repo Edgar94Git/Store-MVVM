1c753559b2505fc38fc64ab42c3531401679a4c5
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-02 13:38:17 -0500

    Listas con Gson y TtypeToken

--- app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
+++ app/src/main/java/com/example/stores/mainModule/model/MainInteractor.kt
@@ -6,6 +6,8 @@ import com.android.volley.toolbox.JsonObjectRequest
 import com.example.stores.StoreApplication
 import com.example.stores.common.entities.StoreEntity
 import com.example.stores.common.utils.Constants
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.getStackTraceString
 import org.jetbrains.anko.uiThread
@@ -15,7 +17,15 @@ class MainInteractor {
     fun getStores(callback: (MutableList<StoreEntity>) -> Unit){
         val url = Constants.STORES_URL + Constants.GET_ALL_PATH
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
-            Log.i("peticon", response.toString())
+            val status = response.getInt(Constants.STATUS_PROPERTY)
+            if(status == Constants.SUCCESS){
+               /* val jsonObject = Gson().fromJson<StoreEntity>(
+                    response.getJSONArray(Constants.STORES_PROPERTY).get(0).toString(), StoreEntity::class.java)*/
+                val jsonList = response.getJSONArray(Constants.STORES_PROPERTY).toString()
+                val mutableListType = object : TypeToken<MutableList<StoreEntity>>(){}.type
+                val storeList = Gson().fromJson<MutableList<StoreEntity>>(jsonList, mutableListType)
+                callback(storeList)
+            }
         }, {
             it.printStackTrace()
         })
