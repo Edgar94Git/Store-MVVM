7e8253dd5431d652b2a4192a8e830599d4f1f180
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2022-09-06 07:23:54 -0500

    Corrutinas: DAO & Interactor(Edit Module)

--- app/src/main/java/com/example/stores/editModule/model/EditStoreInteractor.kt
+++ app/src/main/java/com/example/stores/editModule/model/EditStoreInteractor.kt
@@ -3,6 +3,8 @@ package com.example.stores.editModule.model
 import androidx.lifecycle.LiveData
 import com.example.stores.StoreApplication
 import com.example.stores.common.entities.StoreEntity
+import com.example.stores.common.utils.StoreException
+import com.example.stores.common.utils.TypeError
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
 import java.util.*
@@ -13,21 +15,13 @@ class EditStoreInteractor {
         return StoreApplication.dataBase.storeDao().getStoreById(id)
     }
 
-    fun saveStore(storeEntity: StoreEntity, callback: (Long) -> Unit){
-        doAsync {
-            val newId =  StoreApplication.dataBase.storeDao().addStore(storeEntity)
-            uiThread {
-                callback(newId)
-            }
-        }
+    suspend fun saveStore(storeEntity: StoreEntity){
+        val result = StoreApplication.dataBase.storeDao().addStore(storeEntity)
+        if(result == 0L) throw StoreException(TypeError.INSERT)
     }
 
-    fun  updateStore(storeEntity: StoreEntity, callback: (StoreEntity) -> Unit){
-        /*doAsync {
-            StoreApplication.dataBase.storeDao().updateStore(storeEntity)
-            uiThread {
-                callback(storeEntity)
-            }
-        }*/
+    suspend fun  updateStore(storeEntity: StoreEntity){
+        val result = StoreApplication.dataBase.storeDao().updateStore(storeEntity)
+        if(result == 0) throw StoreException(TypeError.UPDATE)
     }
 }
